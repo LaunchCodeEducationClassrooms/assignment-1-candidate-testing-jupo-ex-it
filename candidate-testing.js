86977abbda5eedf11217c9caaa78f1ee7181a66c
Justin Portell <72322829+jupo-ex-it@users.noreply.github.com>
2021-02-19 15:07:35 -0600

    Update candidate-testing.js

--- candidate-testing.js
+++ candidate-testing.js
@@ -14,14 +14,14 @@ let questions = [
   "(5 + 3)/2 * 10 = ? ",
   "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
   "What is the minimum crew size for the ISS? "
-  ];
+];
 let correctAnswers = [
   "Sally Ride",
   "true",
   "40",
   "Trajectory",
   "3"
-  ];
+];
 let candidateAnswers = [];
 
 
